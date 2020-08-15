9026fd36095f8268943800969eaf1f427899d650
liudewei <a326703305@qq.com>
2020-08-15 22:57:43 +0800

    change max limit

--- out/tools/CopyHistroyManager.js
+++ out/tools/CopyHistroyManager.js
@@ -2,41 +2,39 @@
 const vscode = require('vscode')
 
 Object.defineProperty(exports, "__esModule", {value: true});
+const HIS_MAX_NUM = 50
 
 class CopyHistroyManager {
     constructor(){
         this._configuration = vscode.workspace.getConfiguration("SuperPathCopy");
 
         this.his_copy = []
-        this.copy_map = {}
+        this.copy_map = new Map();
         this._update_max_num();
         vscode.workspace.onDidChangeConfiguration((event, args)=>{
             this._update_max_num();
         })
     }
-
     _update_max_num(){
         let max_num = this._configuration.get("his_copy_max_num");
-        if(max_num > 30){
-            vscode.window.showWarningMessage("历史记录最高仅支持30条");
-            max_num = 30;
+        if(max_num > HIS_MAX_NUM){
+            vscode.window.showWarningMessage(`历史记录最高仅支持${HIS_MAX_NUM}条`);
+            max_num = HIS_MAX_NUM;
         }
         this.max_his_number = max_num;
     }
-
     add_new_copy(path){
-        if(this.copy_map[path]){
+        if(this.copy_map.has(path)){
             return
         }
-        this.copy_map[path] = true
+        this.copy_map.set(path, true)
         
         this.his_copy.push(path);
         if(this.his_copy.length > this.max_his_number){
             var str = this.his_copy.pop()
-            this.copy_map[str] = null
+            this.copy_map.delete(str)
         }
     }
-
     get_his_copys(){
         return this.his_copy
     }
